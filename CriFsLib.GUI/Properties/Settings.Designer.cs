1cc1ff4d0ee5334062d81cf362bebc233e91ba4f
sophia <neorune2@gmail.com>
2023-11-16 17:46:24 -0500

    typo

--- CriFsLib.GUI/Properties/Settings.Designer.cs
+++ CriFsLib.GUI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CRIPack.Properties {
+namespace CPKPack.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
